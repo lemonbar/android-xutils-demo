a2831ade5e2f7451c0f547b53c880d2c2f41842f
lemon <limengabc@163.com>
2014-09-12 13:43:56 +0800

    http xutils usage

--- src/main/java/com/lemon/android/HelloAndroidActivity.java
+++ src/main/java/com/lemon/android/HelloAndroidActivity.java
@@ -5,8 +5,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import com.lemon.android.model.User;
+import com.lidroid.xutils.BitmapUtils;
+import com.lidroid.xutils.DbUtils;
+import com.lidroid.xutils.HttpUtils;
 import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.exception.DbException;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
 import com.lidroid.xutils.view.annotation.ContentView;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
@@ -31,9 +41,61 @@ public class HelloAndroidActivity extends Activity {
     @ViewInject(R.id.textView)
     private TextView textView;
 
+    @ViewInject(R.id.imageView)
+    private ImageView imageView;
+
     @OnClick(R.id.button)
     public void demo(View view) {
-        textView.setText("button is clicked!");
+        DbUtils db = DbUtils.create(HelloAndroidActivity.this);
+        User u = new User();
+        u.setId(1);
+        u.setAge(5);
+        u.setName("aaa");
+        try {
+            db.save(u);
+            User user = db.findById(User.class, 1);
+            textView.setText("inserted new user name is " + user.getName());
+        } catch (DbException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnClick(R.id.button3)
+    public void demo3(View view) {
+        BitmapUtils bitmap = new BitmapUtils(this);
+        bitmap.display(imageView, "http://bbs.lidroid.com/static/image/common/logo.png");
+    }
+
+    private String url = "http://edu.gehealthcare.cn/api/v1/login?mobile=13333333333&password=333333";
+//    String url = "http://www.baidu.com";
+
+    @OnClick(R.id.button2)
+    public void demo2(View view) {
+        HttpUtils http = new HttpUtils();
+        http.configCurrentHttpCacheExpiry(1000 * 10);
+        http.send(HttpRequest.HttpMethod.GET,
+                url,
+                new RequestCallBack<String>() {
+                    @Override
+                    public void onStart() {
+                        textView.setText("loading...");
+                    }
+
+                    @Override
+                    public void onLoading(long total, long current, boolean isUploading) {
+                        textView.setText(current + "/" + total);
+                    }
+
+                    @Override
+                    public void onSuccess(ResponseInfo<String> responseInfo) {
+                        textView.setText("http response is " + responseInfo.result);
+                    }
+
+                    @Override
+                    public void onFailure(HttpException error, String msg) {
+                        textView.setText("Error in http request " + msg);
+                    }
+                });
     }
 
 }
---
+++ src/main/java/com/lemon/android/model/User.java
@@ -0,0 +1,13 @@
+package com.lemon.android.model;
+
+import lombok.Data;
+
+/**
+ * Created by lemon on 14-9-12.
+ */
+@Data
+public class User {
+    private int id;
+    private String name;
+    private int age;
+}
